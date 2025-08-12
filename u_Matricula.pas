69f5d180ee07db2d738af583413ba030e7f729d3
GJosetti <guilhermejosetti7@gmail.com>
2025-08-12 16:46:44 -0300

    Implementando Metodos e variaveis da classe Matriculas

--- u_Matricula.pas
+++ u_Matricula.pas
@@ -1,7 +1,7 @@
 unit u_Matricula;
 
 interface
-uses u_ID;
+uses u_ID, System.Generics.Collections, u_Estudante, u_Turma, System.SysUtils;
 
 type TMatricula = class(TID)
 
@@ -17,10 +17,15 @@ type TMatricula = class(TID)
   function getIDEstudante : Integer;
   procedure setIDEstudante (aID: Integer);
 
+  function getNomeEstudanteByID() : string;
+  function getNomeTurmaByID: string;
+  procedure setIDEstudanteByNome(aNome: string);
+  procedure setIDTurmaByNome(aNome: string);
+
 
 end;
 
-
+var matriculasList : TObjectList<TMatricula>;
 
 implementation
 
@@ -36,14 +41,73 @@ begin
   Result := Self.IDTurma;
 end;
 
+function TMatricula.getNomeEstudanteByID: string;
+var i: Integer;
+begin
+
+  for i := 0 to matriculasList.Count -1 do
+  begin
+    if(Self.IDEstudante = estudantesList[i].GetCodigo) then
+    begin
+      Result := estudantesList[i].getNome;
+    end;
+  end;
+
+end;
+
+function TMatricula.getNomeTurmaByID: string;
+var i: Integer;
+begin
+
+  for i := 0 to matriculasList.Count -1 do
+  begin
+    if(Self.IDEstudante = turmasList[i].GetCodigo) then
+    begin
+      Result := turmasList[i].GetCodigo.ToString + ' - ' + 'Professor(a) ' + turmasList[i].getNomeProfessorByID + ' -> ' + turmasList[i].getNomeDisciplinaByID;
+    end;
+  end;
+
+end;
+
 procedure TMatricula.setIDEstudante(aID: Integer);
 begin
   Self.IDEstudante := aID;
 end;
 
+procedure TMatricula.setIDEstudanteByNome(aNome: string);
+var i : Integer;
+begin
+  for i := 0 to estudantesList.Count -1  do
+  begin
+    if(aNome = estudantesList[i].getNome) then
+    begin
+      Self.setIDEstudante(estudantesList[i].GetCodigo);
+      break;
+    end;
+  end;
+end;
+
 procedure TMatricula.setIDTurma(aID: Integer);
 begin
   Self.IDTurma := aID;
+end;
+
+procedure TMatricula.setIDTurmaByNome(aNome: string);
+begin
+
+var i : Integer;
+begin
+
+  for i := 0 to turmasList.Count -1  do
+  begin
+    if(aNome = (turmasList[i].GetCodigo).ToString + ' - ' + 'Professor(a) ' + turmasList[i].getNomeProfessorByID + ' -> ' + turmasList[i].getNomeDisciplinaByID)   then
+    begin
+      Self.SetIDTurma(turmasList[i].GetCodigo);
+      break;
+    end;
+  end;
+end;
+
 end;
 
 end.
